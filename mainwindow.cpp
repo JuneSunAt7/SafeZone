9e40875bdfa0d06f1816ee552a381580196a7f62
JuneSunAt7 <jprogram@bk.ru>
2023-04-02 13:58:27 +0300

    FULL CHECK & REPORTS

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,8 @@
 #include <QHostAddress>
 #include <QScrollBar>
 #include <QTextCodec>
+#include <QSysInfo>
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -19,6 +21,11 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     QTextCursor cursor( ui->textEdit->textCursor() );
     ui->textEdit->setFocusPolicy(Qt::NoFocus);
+    ui->progressBar->setStyleSheet("border-radius:20px;"
+                                   "font: 700 22pt 'Segoe UI';"
+                                   "background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 254, 209, 255), stop:1 rgba(0, 172, 234, 255));"
+                                   "selection-background-color: blue;");
+    ui->progressBar->setVisible(false);
 }
 
 MainWindow::~MainWindow()
@@ -31,7 +38,7 @@ void MainWindow::on_pushButton_clicked()
 {
     bool virus = false;
 
-    QDirIterator itDirs(ui->lineEdit->text(), QDir::Dirs);
+    QDirIterator itDirs(ui->lineEdit->text(), QDir::Dirs );
     QString text;
     QString consister = ""; //read from signature txt and consist every str with readable file
     QStringList conOut;
@@ -53,6 +60,8 @@ void MainWindow::on_pushButton_clicked()
 
              reads->open(QFile::ReadOnly);
             for(int i = 0; i < conOut.size(); i++){ // read all str and consist with hex-file
+                    ui->progressBar->setVisible(true);
+                    ui->progressBar->setValue(i);
              if(reads->readLine().toHex() == conOut.at(i)){
                  text.append("          " +info.fileName() +"   -- virus!!    \n" );
                  qDebug() << "          " +info.fileName() +"   -- virus!!    \n";
@@ -71,6 +80,7 @@ void MainWindow::on_pushButton_clicked()
 
              reads->close();
       }
+          ui->progressBar->setVisible(false);
       if(text.size() != 0){
 
           QColor color = ui->textEdit->textColor();
@@ -92,13 +102,91 @@ void MainWindow::on_pushButton_clicked()
 
 void MainWindow::on_genReportsBth_clicked()
 {
+    QString filename = QDateTime::currentDateTime().toString("ddMMyyyy HHmmss") + "safe_zone_repo.szr";
+
+   QFile *report = new QFile(filename);
+   report->open(QFile::WriteOnly);
+   QString data;
+   QSysInfo info;
+   data.append("SYSTEM\n");
+
+   data.append(   "Build CPU architecture: "+info.buildCpuArchitecture() + "\n" +
+            "Kernel: " + info.kernelType() + info.kernelVersion() + "\n" +
+            "Machine Unique id: "  + info.machineUniqueId() + "\n" +
+            "Product: " + info.prettyProductName() + " " + info.productType() + " " + info.productVersion() + "\n");
+
+   data.append("OPENED PORTS\n");
+
+   QString program = "netstat -a";
+
+   QProcess *Process = new QProcess(this);
+   Process->start(program);
+   Process->waitForFinished();
+   QString output = Process->readAll();
+   output.remove("\r\n\xEF\xBF\xBD\xEF\xBF\xBD\xE2\xA8\xA2\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD \xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xE7\xA5\xAD\xEF\xBF\xBD\xEF\xBF\xBD\r\n\r\n  \xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD    \xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD \xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD        \xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xE8\xAD\xA8\xEF\xBF\xBD \xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD          \xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xAD\xA8\xEF\xBF\xBD\r\n");
+
+
+   data.append(output);
+
+
+   report->write(data.toUtf8());
+
+
+   report->close();
+
+   delete report;
+
+   ui->textEdit->setPlainText("Report saved as " + filename + "\n" + "You can open with notepad.");
 
 }
 
 
 void MainWindow::on_fullCheckBth_clicked()
 {
+    QString text;
+    QString consister = ""; //read from signature txt and consist every str with readable file
+    QStringList conOut;
+    QFile *sign = new QFile("sign1.txt");
 
+    sign->open(QFile::ReadOnly);
+    while (!sign->atEnd()) {
+        conOut.append(sign->readLine());
+    }
+    sign->close();
+
+
+    QDirIterator it(ui->lineEdit->text(),  QDir::NoFilter, QDirIterator::Subdirectories);
+    while (it.hasNext()) {
+        QFile f(it.next());
+        f.open(QIODevice::ReadOnly);
+
+        for(int i = 0; i < conOut.size(); i++){ // read all str and consist with hex-file
+                ui->progressBar->setVisible(true);
+                ui->progressBar->setValue(i);
+         if(f.readLine().toHex() == conOut.at(i)){
+             text.append("          " +f.fileName() +"   -- virus!!    \n" );
+             qDebug() << "          " +f.fileName() +"   -- virus!!    \n";
+
+         }
+
+        }
+        f.close();
+    }
+        ui->progressBar->setVisible(false);
+    if(text.size() != 0){
+
+        QColor color = ui->textEdit->textColor();
+        ui->textEdit->setTextColor(Qt::red);
+         ui->textEdit->setText(text);
+        ui->textEdit->setTextColor(color);
+    }
+    else{
+        QColor color = ui->textEdit->textColor();
+        ui->textEdit->setTextColor(Qt::green);
+        ui->textEdit->setText("Dir clean");
+        ui->textEdit->setAlignment(Qt::AlignmentFlag::AlignHCenter | Qt::AlignmentFlag::AlignVCenter);
+        ui->textEdit->setTextColor(color);
+    }
 }
 
 
@@ -110,7 +198,15 @@ void MainWindow::on_settingsBth_clicked()
 
 void MainWindow::on_sysBth_clicked()
 {
+    QSysInfo info;
+    QString data;
 
+    data = "Build CPU architecture: "+info.buildCpuArchitecture() + "\n" +
+            "Kernel: " + info.kernelType() + info.kernelVersion() + "\n" +
+            "Machine Unique id: "  + info.machineUniqueId() + "\n" +
+            "Product: " + info.prettyProductName() + " " + info.productType() + " " + info.productVersion();
+
+    ui->textEdit->setPlainText(data);
 }
 
 
--- sniff.pro
+++ sniff.pro
@@ -23,4 +23,4 @@ FORMS += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
-win32: RC_ICONS = $$PWD/shield.ico
+win32: RC_ICONS = $$PWD/cyber-security.ico
